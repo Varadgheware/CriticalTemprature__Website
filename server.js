45e53d3580e0be2682658672090b1cd3fc2e410e
Varad GHEWARE <165383238+Varadgheware@users.noreply.github.com>
2025-04-22 22:10:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const connection = require('./src/components/db');
 const app = express();
 const port = 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Test route
+//testroutee
 app.get('/test', (req, res) => {
   res.json({ message: 'Server is working!' });
 });
 
-// API endpoint to get all predictions
+// API endpoint fr all predictionss
 app.get('/api/predictions', (req, res) => {
   connection.query('SELECT * FROM prediction', (err, results) => {
     if (err) {
@@ -27,7 +26,7 @@ app.get('/api/predictions', (req, res) => {
   });
 });
 
-// API endpoint to get prediction by molecular formula
+// API endpoint dor formula
 app.get('/api/predictions/:formula', (req, res) => {
   const formula = req.params.formula;
   connection.query('SELECT * FROM prediction WHERE Material = ?', [formula], (err, results) => {
@@ -46,7 +45,7 @@ app.get('/api/predictions/:formula', (req, res) => {
   });
 });
 
-// API endpoint to add new prediction
+// new predictions
 app.post('/api/predictions', (req, res) => {
   const { molecular_formula, temperature } = req.body;
   const query = 'INSERT INTO prediction (Material, Predicted_T_c) VALUES (?, ?)';
